fd2c29d117811724627935e0e4a9e6d4d29f9c96
Black-Mark <jeshuabay@gmail.com>
2023-11-12 15:28:34 +0800

    modified:   admin/data-management/index.php 	deleted:    admin/departments/extra/ace-elements.min.js 	deleted:    admin/departments/extra/ace-extra.min.js 	deleted:    admin/departments/extra/ace-fonts.css 	deleted:    admin/departments/extra/ace-responsive.min.css 	deleted:    admin/departments/extra/ace-rtl.min.css 	deleted:    admin/departments/extra/ace-skins.min.css 	deleted:    admin/departments/extra/ace.min.css 	deleted:    admin/departments/extra/ace.min.js 	deleted:    admin/departments/extra/bootstrap-responsive.min.css 	deleted:    admin/departments/extra/bootstrap.min.css 	deleted:    admin/departments/extra/bootstrap.min.js 	deleted:    admin/departments/extra/buttons.colVis.min.js 	deleted:    admin/departments/extra/buttons.dataTables.min.css 	deleted:    admin/departments/extra/buttons.flash.min.js 	deleted:    admin/departments/extra/buttons.html5.min.js 	deleted:    admin/departments/extra/buttons.print.min.js 	deleted:    admin/departments/extra/dataTables.buttons.min.js 	deleted:    admin/departments/extra/dataTables.colReorder.min.js 	deleted:    admin/departments/extra/dd.css 	deleted:    admin/departments/extra/font-awesome.min.css 	deleted:    admin/departments/extra/font-awesome.min2.css 	deleted:    admin/departments/extra/jquery-1.10.2.min.js 	deleted:    admin/departments/extra/jquery-ui-1.10.3.full.min.css 	deleted:    admin/departments/extra/jquery-ui-1.10.3.full.min.js 	deleted:    admin/departments/extra/jquery.dataTables.min.css 	deleted:    admin/departments/extra/jquery.dataTables.min.js 	deleted:    admin/departments/extra/jquery.dd.js 	deleted:    admin/departments/extra/jszip.min.js 	deleted:    admin/departments/extra/pdfmake.min.js 	deleted:    admin/departments/extra/vfs_fonts.js 	modified:   admin/departments/index.php 	modified:   admin/employee-list/index.php

--- admin/data-management/index.php
+++ admin/data-management/index.php
@@ -97,7 +97,74 @@ if ($tablelist === 'index.php' || $tablelist === 'index.html') {
                         </div>
                     </div>
                 </div>
+
+                <table id="example" class="display" style="width:100%">
+                    <thead>
+                        <tr>
+                            <th>Employee ID</th>
+                            <th>Last Name</th>
+                            <th>First Name</th>
+                            <th>Middle Name</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                        $sql = "SELECT * FROM tbl_useraccounts";
+                        $result = $database->query($sql);
+                        if ($result->num_rows > 0) {
+                            while ($row = $result->fetch_assoc()) {
+                                ?>
+                                <tr>
+                                    <td>
+                                        <?php echo $row['employee_id']; ?>
+                                    </td>
+                                    <td>
+                                        <?php echo $row['lastName']; ?>
+                                    </td>
+                                    <td>
+                                        <?php echo $row['firstName']; ?>
+                                    </td>
+                                    <td>
+                                        <?php echo $row['middleName']; ?>
+                                    </td>
+                                </tr>
+                                <?php
+                            }
+                        }
+                        ?>
+                    </tbody>
+                </table>
+
+                <script>
+                    $(document).ready(function () {
+                        $('#example').DataTable({
+                            "pagingType": "full_numbers",  // Show First, Previous, Page numbers, Next, Last
+                            "pageLength": 10,             // Number of rows per page
+                            "ordering": true,             // Allow column sorting
+                            "searching": true,            // Enable search
+                            "lengthChange": true,        // Show entries per page
+                            "dom": 'lBfrtip',            // Show Buttons for Print, Copy, Excel, CSV, PDF
+                            "buttons": [
+                                'copy', 'excel', 'csv', 'pdf', 'print'
+                            ]
+                        });
+                    });
+                </script>
+
+
+
+                <script src="../../assets/datatables/Buttons-2.4.2/js/dataTables.buttons.min.js"></script>
+                <script src="../../assets/datatables/Buttons-2.4.2/js/buttons.html5.min.js"></script>
+                <script src="../../assets/datatables/Buttons-2.4.2/js/buttons.print.min.js"></script>
+
+                <script src="../../assets/datatables/JSZip-3.10.1/jszip.min.js"></script>
+                <script src="../../assets/datatables/pdfmake-0.2.7/pdfmake.js"></script>
+                <script src="../../assets/datatables/pdfmake-0.2.7/pdfmake.min.js"></script>
+                <script src="../../assets/datatables/pdfmake-0.2.7/vfs_fonts.js"></script>
+                <script src="../../assets/datatables/datatables.min.js"></script>
+
                 
+
             </div>
         </div>
     </div>
