aaa5e567e5ab4c41a4ac7b45a83c1ecb8a908d2c
Black-Mark <jeshuabay@gmail.com>
2023-11-20 11:00:04 +0800

    modified:   admin/employee-list/leave-data-form/index.php

--- admin/employee-list/leave-data-form/index.php
+++ admin/employee-list/leave-data-form/index.php
@@ -351,25 +351,9 @@ if ($selectedYear) {
                                         <!-- <tr key=""> -->
                                         <tr>
                                             <td class="table-item-base">
-                                                <button type="button" id="createInitialRecord" class="custom-regular-button"
-                                                    data-toggle="modal" data-target="#addLeaveDataRecord">
-                                                    Add New Leave Record
-                                                </button>
-                                                <button class="btn btn-primary" type="button" data-bs-toggle="collapse"
-                                                    data-bs-target="#collapseExample" aria-expanded="false"
-                                                    aria-controls="collapseExample">
-                                                    Button with data-bs-target
-                                                </button>
-                                                <div class="collapse" id="collapseExample">
-                                                    <div class="card card-body">
-                                                        Some placeholder content for the collapse component. This panel is
-                                                        hidden by default but revealed when the user activates the relevant
-                                                        trigger.
-                                                    </div>
-                                                </div>
                                                 <?php echo $ldata['period'];
-                                                if ($ldata['periodEnd'] && $ldata['period'] < $ldata['periodEnd']) {
-                                                    echo ' to ' . $ldata['periodEnd'];
+                                                if($ldata['periodEnd'] && $ldata['period'] < $ldata['periodEnd']){
+                                                    echo ' to '.$ldata['periodEnd'];
                                                 }
                                                 ?>
                                             </td>
@@ -548,7 +532,7 @@ if ($selectedYear) {
                             var floatingPeriodEndValue = $('#floatingPeriodEnd').val();
 
                             // Compare the values
-                            if (floatingPeriodValue >= floatingPeriodEndValue + 1) {
+                            if (floatingPeriodValue >= floatingPeriodEndValue+1) {
                                 if (containerPeriod) {
                                     $('#floatingPeriod').val(containerPeriod);
                                 } else {
