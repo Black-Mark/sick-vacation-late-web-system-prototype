da696880b51ad025ad9abb1f90eb06716b7dd3ac
Black-Mark <jeshuabay@gmail.com>
2024-06-05 13:55:39 +0800

    modified:   actions/removeCacheLateRecord.php

--- actions/removeCacheLateRecord.php
+++ actions/removeCacheLateRecord.php
@@ -7,25 +7,28 @@ include ($constants_variables);
 
 if (isset($_POST['clearCacheLateRecord'])) {
     // Define the directory where the CSV files are stored using relative path
-    $directory = "../files/upload/laterecords";
+    $directory = realpath("../files/upload/laterecords");
 
     // Fetch all file paths from the fileOfRecord column in the tbl_laterecordfile table
     $query = "SELECT fileOfRecord FROM tbl_laterecordfile";
     $result = mysqli_query($database, $query);
 
     if ($result) {
-        // Store all file paths from the database in an array
+        // Store all file paths from the database in an array and convert to absolute paths
         $db_files = [];
         while ($row = mysqli_fetch_assoc($result)) {
-            $db_files[] = str_replace('\\', '/', $row['fileOfRecord']);
+            if (!empty($row['fileOfRecord'])) {
+                // Convert to absolute path and use forward slashes
+                $db_files[] = str_replace('\\', '/', realpath("../" . $row['fileOfRecord']));
+            }
         }
 
         // Scan the directory for all CSV files
         $all_files = glob($directory . "/*.csv");
 
-        // Convert all files to relative paths
+        // Convert directory paths to use forward slashes
         foreach ($all_files as $key => $file) {
-            $all_files[$key] = str_replace('\\', '/', str_replace(realpath('../'), '', realpath($file)));
+            $all_files[$key] = str_replace('\\', '/', $file);
         }
 
         // Debugging output: print the arrays
@@ -37,18 +40,26 @@ if (isset($_POST['clearCacheLateRecord'])) {
 
         // Loop through all files in the directory
         foreach ($all_files as $file) {
-            // Check if the file is not listed in the database
+            echo "Checking file: " . $file . "<br>";
+
             if (!in_array($file, $db_files)) {
-                // // Delete the file
-                // if (unlink(realpath($directory . '/' . basename($file)))) {
-                //     echo "Deleted: " . $file . "<br>";
-                // } else {
-                //     echo "Error deleting: " . $file . "<br>";
-                // }
+                echo "Will delete: " . $file . "<br>";
+
+                // Delete the file
+                if (unlink($file)) {
+                    echo "Deleted: " . $file . "<br>";
+                } else {
+                    echo "Error deleting: " . $file . "<br>";
+                }
+            } else {
+                echo "Will not delete: " . $file . "<br>";
             }
         }
+        $_SESSION['alert_message'] = "Clearing Cache Files Success!";
+        $_SESSION['alert_type'] = $success_color;
     } else {
-        echo "Error fetching records from database.";
+        $_SESSION['alert_message'] = "Clearing of Unwanted Files Failed!";
+        $_SESSION['alert_type'] = $error_color;
     }
 }
 ?>
